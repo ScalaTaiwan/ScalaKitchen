dfb6025cedcedcd31c48cb7b7a83b4930e15bb44
Steven <kenshin520@gmail.com>
2017-10-31 10:01:25 +0800

    make "view bound" deprecated and related adjustments according to review comments

---
+++ src/main/scala/scalataiwan/snippet/PackageImplicit.scala
@@ -0,0 +1,15 @@
+package a {
+  class A (val n: Int)
+}
+ 
+package object a {
+  implicit def aToString(a: A): String = s"A: ${a.n}"
+}
+ 
+object Test {
+  import a._
+  def main(s: Array[String]) = {
+    val as: String = new A(100)
+    println(as)
+  }
+}
